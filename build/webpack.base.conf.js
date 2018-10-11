114e5c5317c5c3e7dc134d8f20276d5c062bf679
Abhishek Jaiswal <abhishekjaiswal@Abhisheks-MacBook-Pro.local>
2018-10-11 19:42:15 +0530

    ctx

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -41,7 +41,11 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env']
+            presets: ['@babel/preset-env'],
+            plugins: [
+              '@babel/plugin-transform-runtime',
+              '@babel/plugin-proposal-async-generator-functions'
+            ]
           }
         }
       },
