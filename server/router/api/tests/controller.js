76d2c0b7765b2318e4e44f01e91a4f652e108733
Abhishek Jaiswal <abhishekjaiswal@Abhisheks-MacBook-Pro.local>
2018-10-18 21:59:39 +0530

    ctx

--- server/router/api/tests/controller.js
+++ server/router/api/tests/controller.js
@@ -5,6 +5,9 @@ module.exports = {
     ctx.body = ['list of all test files']
   },
   async scheduler_register (ctx) {
-    scheduler.register('1-59/1 * * * *', { job_id: ctx.request.fields.job_id, timezone: 'America/New_York' })
+    scheduler.register('1-59/1 * * * *', {
+      job_id: ctx.request.fields.job_id,
+      timezone: 'America/New_York'
+    })
   }
 }
