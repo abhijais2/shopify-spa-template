21395d45d9ef008f54017d10234167c362e6d7ac
Abhishek Jaiswal <abhi.jaiswal.02.git@gmail.com>
2018-10-21 13:37:12 +0530

    ctx

--- server/router/api/index.js
+++ server/router/api/index.js
@@ -2,13 +2,11 @@ const Router = require('koa-router')
 const router = new Router()
 
 const tests = require('./tests')
-const alerts = require('./alerts')
 
 router.all('*', async (ctx, next) => {
   await next()
 })
 
 router.use(tests)
-router.use(alerts)
 
 module.exports = router
